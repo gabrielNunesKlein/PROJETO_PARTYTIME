13b777f45bb5e128dc491ef873beefbb701efbe1
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-10-10 14:26:30 -0300

    Setup inicial

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+// modules
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// routes
+
+// middlewares
+
+// config
+const dbName = "partytime";
+const port = 3000;
+
+// coxao com mongodb
+mongoose.connect(
+    `mongodb://localhost/${dbName}`,
+    {
+        useNewUrlParser: true,
+        //useFindAndModify: false,
+        useUnifiedTopology: true
+    }
+);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.json({ message: "Rota teste"});
+});
+
+app.listen(port, function(){
+    console.log("Backend está rodando na porta: ", port);
+});
