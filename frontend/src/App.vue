843806a3f79832655fa58c313f465e48c849edab
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-10-15 15:50:50 -0300

    Criando NavBar e Footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <NavBar />
+    <router-view class="container" />
+    <Footer />
   </div>
-  <router-view/>
 </template>
 
+<script>
+import NavBar from "./components/NavBar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  name: "app",
+  components: {
+    NavBar,
+    Footer
+  }
+}
+
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Helvetica;
+    color: #444;
+  }
 
-#nav {
-  padding: 30px;
-}
+  .container{
+    min-height: 300px;
+  } carteirinha
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  a {
+    transition: .5s;
+  }
 </style>
---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+<div id="nav">
+    <router-link to="/" id="logo-container">
+        <img src="/img/partytimelogo.png" alt="Party Time">
+    </router-link>
+    <h2 id="nav-title">Paty Time</h2>
+    <div id="nav-links">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">Entrar</router-link>
+        <router-link to="/about">Cadastrar</router-link>
+    </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: "NavBar"
+}
+</script>
+
+
+<style scoped>
+    #nav {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        border-bottom: 1px solid #EEE;
+    }
+
+    #nav a {
+        text-decoration: none;
+        margin-right: 15px;
+    }
+
+    #nav img {
+        width: 35px;
+    }
+
+    #nav-title {
+        font-size: 3rem;
+        font-weight: 300;
+    }
+
+    #logo-container, #nav-links{
+        width: 400px;
+    }
+
+    #nav-links {
+        display: flex;
+        justify-content: flex-end;
+    }
+
+    button{
+        background-color: #FFF;
+        border: none;
+        font-size: 16px;
+        cursor: pointer;
+    }
+
+    bottom:hover{
+        color: #C1B696;
+    }
+</style>
